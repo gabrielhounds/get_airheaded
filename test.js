986b02ad644a93ba2291374ab746d03d9304b3a5
Patrick Batey <pbatey@patrickBatey-macBook.local>
2018-06-17 15:32:09 -0400

    Sparkflow updates

--- test.js
+++ test.js
@@ -17,6 +17,12 @@ function init() {
 	var tlEndOut 	= new TimelineMax({paused:true});
 	var tlGameOver 	= new TimelineMax({paused:true});
 
+	Number.isInteger = Number.isInteger || function(value) {
+    return typeof value === "number" &&
+           isFinite(value) &&
+           Math.floor(value) === value;
+	};
+
 	Text = (function() {
 		var interfaceTextStyle = new PIXI.TextStyle({
 			align : 'center',
@@ -206,6 +212,12 @@ function init() {
 		}
 	}());
 
+	var isMobile = (function () {
+		var check = false;
+		(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+		return check;
+	})();
+
 	function initAudio() {
 		bgSound = new Howl({
 			src : ['https://c1.undertonevideo.com/clients/Airheads/sounds/bg-sound.mp3'],
@@ -257,8 +269,11 @@ function init() {
 	}
 
 	function initStage() {
+
 		game = $('<div>', {id:'game'}).appendTo('body');
 		closeBtn = $('<div>', {id : 'closeBtn'}).appendTo(game);
+
+
 		if (_width >= 1280 ) {
 			screenSize = 'desktop';
 			$(game).css({width:1280, height:500});
@@ -268,6 +283,7 @@ function init() {
 			$(game).css({width:'100%', height:500});
 			app = new Application({width : _width, height : 500, forceCanvas : true});
 		} else if ( _width < 768 ) {
+
 			screenSize = 'mobile';
 			$(game).css({width:'100%', height:'100%'});
 			app = new Application({width : _width, height : _height, forceCanvas : true});
@@ -284,6 +300,7 @@ function init() {
 				screenHeight = 'normal';
 			}
 		}
+
 		app.renderer.backgroundColor = 0x0040A3;
 		$(app.view).appendTo(game);
 		stageW = app.renderer.view.width;
@@ -1407,11 +1424,74 @@ function init() {
 		}
 	});
 
-	var isMobile = (function () {
-		var check = false;
-		(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-		return check;
-	})();
+	var adClose = false;
+  	var n_width, n_height, n_orientation;
+  	var resized = false;
+  	var resizeTimer;
+    var orientation = Math.abs(window.orientation);
+
+	function resetResizeTimer() {
+      setTimeout( function() {
+          clearTimeout(resizeTimer);
+          resized = false;
+      }, 1000);
+    }
+
+	if  (isMobile === true &&  Math.abs(window.orientation) === 90 ) {
+		//alert('landscape view');
+      	t.set(rotateAlertOverlay, {autoAlpha:1});
+    }
+
+	window.addEventListener("orientationchange", function() {
+      	n_orientation = Math.abs(window.orientation);
+      	if  (isMobile === true &&  Math.abs(window.orientation) === 90 ) {
+			//alert('landscape view');
+      		t.set(rotateAlertOverlay, {autoAlpha:1});
+
+        	if (playing === true) {
+
+         		ticker.stop();
+              	Howler.volume(0.0);
+        	}
+    	}
+      	if (isMobile === true && window.orientation === 0) {
+          t.set(rotateAlertOverlay, {autoAlpha:0});
+          if (playing === true) {
+			setTimeout( function() {
+              ticker.start();
+              Howler.volume(0.5);
+            }, 500);
+          }
+        }
+    });
+
+	$(document).on('adStateChange', function () {
+      log("document.on adStateChange fired");
+      log("adStateChange is: " + mraid.getState());
+      if (mraid.getState() == "resized") {
+        	adClose = false;
+      		Howler.volume(0.5);
+         	t.to('#game', 0.2, {opacity:1});
+         	t.to('#game', 0.5, {height:_height});
+      }
+      if (mraid.getState() == "default") {
+        	adClose = true;
+            t.set(rotateAlertOverlay, {autoAlpha:0});
+        	t.to('#game', 1, {height:0});
+        	t.to('#game', 1, {opacity:0, delay:0.5});
+        	Howler.volume(0.0);
+        	if (introPlaying === true){
+          		introTicker.stop();
+       	 	}
+        	if (playing === true) {
+           		lives = 0;
+           		ticker.stop();
+           		handleGameOver(false);
+        	}
+      }
+    });
+
+
 	initStage();
 }
 //
